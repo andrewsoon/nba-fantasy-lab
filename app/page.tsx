6544cb7d6bf0779fd323107e5be9b8d6a858a575
Andrew Soon <andrewsoon.tx@gmail.com>
2025-12-02 13:43:24 +0800

    Table UI improvements

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
         <div className="flex flex-col items-center gap-6 py-24 pb-16 px-8 sm:py-28 sm:pb-16 sm:px-12 md:py-32 md:pb-16 md:px-15">
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl">NBA Fantasy Simulator</h1>
           <div className="flex flex-col items-center gap-2 text-center">
-            <h3 className="text-xl font-semibold">
+            <h3 className="text-lg font-semibold">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce consequat rutrum consectetur.
             </h3>
-            <p className="text-lg">
+            <p className="text-md">
               Phasellus justo nisl, fringilla et nisi quis, posuere sagittis justo. Quisque sed risus ut dui feugiat tempus eu quis ipsum. Phasellus vestibulum erat leo, sed venenatis dui volutpat sed.
             </p>
           </div>
--- app/pageComponents.tsx
+++ app/pageComponents.tsx
@@ -8,43 +8,76 @@ interface PlayersTableProps {
 export const PlayersTable = ({ players }: PlayersTableProps) => {
   return (
     <div className="overflow-x-auto px-8 sm:px-12 md:px-15 pb-16">
-      <table className="min-w-full border ">
+      <table className="min-w-full border border-gray-200 dark:border-gray-700 ">
         <thead className="bg-zinc-100 dark:bg-zinc-800">
           <tr>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">Player</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">Team</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">GP</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">FG%</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">FT%</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">3PM</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">PTS</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">AST</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">REB</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">STL</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">BLK</th>
-            <th className="border px-4 py-2 text-left text-sm font-semibold">TOV</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">Rk</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">Player</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">Team</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">GP</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">FG%</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">FT%</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">3PM</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">PTS</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">AST</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">REB</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">STL</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">BLK</th>
+            <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">TOV</th>
           </tr>
         </thead>
         <tbody>
-          {players.map((player) => {
+          {players.map((player, id) => {
             return (
-              <tr key={`${player.id}-stats`}>
-                <td className="border px-4 py-2 text-sm flex flex-row items-center gap-2">
-                  <img src={`https://cdn.nba.com/headshots/nba/latest/260x190/${player.id}.png`} alt={`${player.id}-headshot`} className="h-10" />
-                  <p>{player.name}</p>
-                </td>
-                <td className="border px-4 py-2 text-sm">{player.team}</td>
-                <td className="border px-4 py-2 text-sm">{player.games_played}</td>
-                <td className="border px-4 py-2 text-sm">{player.fg_pct}</td>
-                <td className="border px-4 py-2 text-sm">{player.ft_pct}</td>
-                <td className="border px-4 py-2 text-sm">{player.fg3m}</td>
-                <td className="border px-4 py-2 text-sm">{player.pts}</td>
-                <td className="border px-4 py-2 text-sm">{player.reb}</td>
-                <td className="border px-4 py-2 text-sm">{player.ast}</td>
-                <td className="border px-4 py-2 text-sm">{player.stl}</td>
-                <td className="border px-4 py-2 text-sm">{player.blk}</td>
-                <td className="border px-4 py-2 text-sm">{player.tov}</td>
-              </tr>
+              <>
+                <tr key={`${player.id}-stats`}>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{id + 1}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">
+                    <div className="flex flex-row items-center gap-2">
+                      <img src={`https://cdn.nba.com/headshots/nba/latest/260x190/${player.id}.png`} alt={`${player.id}-headshot`} className="h-10" />
+                      <p>{player.name}</p>
+                    </div>
+                  </td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.team}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.games_played}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">
+                    <div className="flex flex-row items-center gap-1 h-full">
+                      <p>{player.fg_pct}</p>
+                      <p className="text-xs text-gray-600 dark:text-gray-400">({player.fgm}/{player.fga})</p>
+                    </div>
+                  </td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">
+                    <div className="flex flex-row items-center gap-1 h-full">
+                      <p>{player.ft_pct}</p>
+                      <p className="text-xs text-gray-600 dark:text-gray-400">({player.ftm}/{player.fta})</p>
+                    </div>
+                  </td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.fg3m}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.pts}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.reb}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.ast}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.stl}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.blk}</td>
+                  <td className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-sm">{player.tov}</td>
+                </tr>
+                {(id + 1) % 15 === 0 && (
+                  <tr key={id} className="bg-zinc-100 dark:bg-zinc-800">
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">Rk</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">Player</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">Team</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">GP</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">FG%</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">FT%</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">3PM</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">PTS</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">AST</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">REB</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">STL</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">BLK</th>
+                    <th className="border border-gray-200 dark:border-gray-700 px-4 py-2 text-left text-sm font-semibold">TOV</th>
+                  </tr>
+                )}
+              </>
             )
           })}
         </tbody>
