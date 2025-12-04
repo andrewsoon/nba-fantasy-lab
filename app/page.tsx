1d0e44d32e21ed1688def77da90112462007a349
Andrew Soon <andrewsoon.tx@gmail.com>
2025-12-05 00:28:02 +0800

    Copy changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,17 @@ export default function Home() {
           <svg xmlns="http://www.w3.org/2000/svg" className="w-5 sm:w-10 h-5 sm:h-10" fill="none" viewBox="0 0 24 24" stroke="#FFBF00">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2a9.99 9.99 0 016.364 2.364A10 10 0 0112 22a9.99 9.99 0 01-6.364-2.364A10 10 0 0112 2zm0 0v20m-7.071-7.071h14.142M4.929 4.929c4.686 4.686 4.686 9.456 0 14.142M19.071 4.929c-4.686 4.686-4.686 9.456 0 14.142" />
           </svg>
-          NBA Fantasy Simulator
+          NBA Fantasy Lab
           <svg xmlns="http://www.w3.org/2000/svg" className="w-5 sm:w-10 h-5 sm:h-10" fill="none" viewBox="0 0 24 24" stroke="#FFBF00">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2a9.99 9.99 0 016.364 2.364A10 10 0 0112 22a9.99 9.99 0 01-6.364-2.364A10 10 0 0112 2zm0 0v20m-7.071-7.071h14.142M4.929 4.929c4.686 4.686 4.686 9.456 0 14.142M19.071 4.929c-4.686 4.686-4.686 9.456 0 14.142" />
           </svg>
         </h1>
         <div className="flex flex-col items-center gap-2 text-center">
           <h3 className="text-md sm:text-2xl font-semibold text-zinc-700 dark:text-zinc-200">
-            Season 2025-26
+            Explore, Analyse, and Build Your Winning Strategy
           </h3>
-          <p className="text-sm sm:text-lg text-zinc-700 dark:text-zinc-300 ">
-            Showing stats for all active players. Click a column to sort.
+          <p className="text-sm sm:text-lg text-zinc-700 dark:text-zinc-300 max-w-250">
+            Use interactive heatmaps to spot category strengths, compare trends across time windows, and experiment with roster builds designed to maximise weekly H2H edges in your fantasy matchups.
           </p>
         </div>
       </div>
